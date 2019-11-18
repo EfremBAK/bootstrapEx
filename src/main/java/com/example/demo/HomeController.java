001c260a76e455d21710797343d81c2cb0b80cf2
EfremBAK <efrem.berhane2009@gmail.com>
2019-11-18 16:51:25 -0500

    second day

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index(){
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("myvar", "Say hello to my lil friend");
         return "index";
     }
+
     @GetMapping("/git")
     public String git(){
         return "git";
